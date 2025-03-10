8f486427a75ea239be0a77eca6b7db4b7d3101e4
Hans Landgrebe <hlxbl20@gmail.com>
2025-03-10 12:43:48 -0500

    Defined Vector2 struct

--- main.c
+++ main.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
+struct Vector2 {
+    float x;
+    float y;
+} typedef Vector2;
+
 int main(int argc, char* argv[]) {
+
+    Vector2 gameBoarder;
+    gameBoarder.x = 960;
+    gameBoarder.y = 520;
+
+    printf("%f %f", gameBoarder.x, gameBoarder.y);
     return 0;
 }
