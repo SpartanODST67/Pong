315df18cd4f967d0981e796de4558efe6147247f
Hans Landgrebe <hlxbl20@gmail.com>
2025-03-10 15:58:57 -0500

    Moved Pong logic to WinMain

--- main.c
+++ main.c
@@ -9,6 +9,9 @@ LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 const char windowClassName[] = "PongWindowClass";
 
 /*int main(int argc, char* argv[]) {    
+}*/
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
     
     Vector2 gameBorder;
     gameBorder.x = 960;
@@ -33,59 +36,6 @@ const char windowClassName[] = "PongWindowClass";
     rightPaddle.dimensions.y = ball.dimensions.y * 4;
     rightPaddle.position.x = gameBorder.x - 40;
     rightPaddle.position.y = gameBorder.y / 2;
-
-    HDC screen = GetDC(NULL);
-    if(screen == NULL) {
-        fprintf(stderr, "Failed to get screen context.\n");
-        return -1;
-    }
-
-    for(int i = 0; i < 100; i++) {
-        ball.position = moveBall(&ball, leftPaddle, rightPaddle, gameBorder);
-        //printf("Game dimensions: %fx%f, Ball at: (%f, %f)\n", gameBorder.x, gameBorder.y, ball.position.x, ball.position.y);
-        //drawScreen(screen, gameBorder, ball, leftPaddle, rightPaddle);
-        Sleep((1.0f/60.0f) * 1000.0f);
-    }
-    return 0;
-}*/
-
-/*void drawScreen(HDC screen, Vector2 dimensions, Block ball, Block leftPaddle, Block rightPaddle) {
-    //Draw background
-    HBRUSH brush = CreateSolidBrush(RGB(0,0,0));
-    SelectObject(screen, brush);
-    Rectangle(screen, 0, 0, dimensions.x, dimensions.y);
-
-    brush = CreateSolidBrush(RGB(255, 255, 255));
-    SelectObject(screen, brush);
-    //Draw Ball
-    Rectangle(screen, ball.position.x - (ball.dimensions.x / 2), (ball.position.y + ball.dimensions.y / 2),
-                ball.position.x + (ball.dimensions.x / 2), ball.position.y - (ball.dimensions.y / 2));
-
-    //Draw Left Paddle
-    Rectangle(screen, leftPaddle.position.x - (leftPaddle.dimensions.x / 2), (leftPaddle.position.y + leftPaddle.dimensions.y / 2),
-                leftPaddle.position.x + (leftPaddle.dimensions.x / 2), leftPaddle.position.y - (leftPaddle.dimensions.y / 2));
-
-    //Draw Right Paddle
-    Rectangle(screen, rightPaddle.position.x - (rightPaddle.dimensions.x / 2), (rightPaddle.position.y + rightPaddle.dimensions.y / 2),
-        rightPaddle.position.x + (rightPaddle.dimensions.x / 2), rightPaddle.position.y - (rightPaddle.dimensions.y / 2));
-}*/
-
-LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
-    switch(msg) {
-        case WM_CLOSE:
-            DestroyWindow(hwnd);
-            break;
-        case WM_DESTROY:
-            PostQuitMessage(0);
-            break;
-        default:
-        return DefWindowProc(hwnd, msg, wParam, lParam);
-    }
-    return 0;
-}
-
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-    
     
     WNDCLASSEX wc;
     HWND hwnd;
@@ -117,7 +67,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
         windowClassName,
         "Pong",
         WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT, CW_USEDEFAULT, 240, 120,
+        CW_USEDEFAULT, CW_USEDEFAULT, gameBorder.x, gameBorder.y,
         NULL, NULL, hInstance, NULL
     );
 
@@ -130,10 +80,49 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     ShowWindow(hwnd, nCmdShow);
     UpdateWindow(hwnd);
 
+    HDC screen = GetDC(hwnd);
+    if(screen == NULL) {
+        fprintf(stderr, "Failed to get screen context.\n");
+        return -1;
+    }
+
     while(GetMessage(&msg, NULL, 0, 0) > 0) {
         TranslateMessage(&msg);
+        ball.position = moveBall(&ball, leftPaddle, rightPaddle, gameBorder);
+        drawScreen(screen, gameBorder, ball, leftPaddle, rightPaddle);
+        //Sleep((1.0f/60.0f) * 1000.0f);
         DispatchMessage(&msg);
     }
 
     return msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    switch(msg) {
+        case WM_CLOSE:
+            DestroyWindow(hwnd);
+            break;
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            break;
+        default:
+        return DefWindowProc(hwnd, msg, wParam, lParam);
+    }
+    return 0;
+}
+
+void drawScreen(HDC screen, Vector2 dimensions, Block ball, Block leftPaddle, Block rightPaddle) {
+    HBRUSH brush = CreateSolidBrush(RGB(255, 255, 255));
+    SelectObject(screen, brush);
+    //Draw Ball
+    Rectangle(screen, ball.position.x - (ball.dimensions.x / 2), (ball.position.y + ball.dimensions.y / 2),
+                ball.position.x + (ball.dimensions.x / 2), ball.position.y - (ball.dimensions.y / 2));
+
+    //Draw Left Paddle
+    Rectangle(screen, leftPaddle.position.x - (leftPaddle.dimensions.x / 2), (leftPaddle.position.y + leftPaddle.dimensions.y / 2),
+                leftPaddle.position.x + (leftPaddle.dimensions.x / 2), leftPaddle.position.y - (leftPaddle.dimensions.y / 2));
+
+    //Draw Right Paddle
+    Rectangle(screen, rightPaddle.position.x - (rightPaddle.dimensions.x / 2), (rightPaddle.position.y + rightPaddle.dimensions.y / 2),
+        rightPaddle.position.x + (rightPaddle.dimensions.x / 2), rightPaddle.position.y - (rightPaddle.dimensions.y / 2));
 }
