8572c20fba3c09aea229752ff5caafe7badfbe35
Hans Landgrebe <hlxbl20@gmail.com>
2025-03-10 12:52:43 -0500

    Created Block struct

--- main.c
+++ main.c
@@ -5,12 +5,23 @@ struct Vector2 {
     float y;
 } typedef Vector2;
 
+struct Block {
+    Vector2 position;
+    Vector2 dimensions;
+} typedef Block;
+
 int main(int argc, char* argv[]) {
 
     Vector2 gameBoarder;
     gameBoarder.x = 960;
     gameBoarder.y = 520;
 
-    printf("%f %f", gameBoarder.x, gameBoarder.y);
+    Block ball;
+    ball.dimensions.x = 10;
+    ball.dimensions.y = 10;
+    ball.position.x = (gameBoarder.x / 2);
+    ball.position.y = (gameBoarder.y / 2);
+
+    printf("Game dimensions: %fx%f, Ball at: (%f, %f)", gameBoarder.x, gameBoarder.y, ball.position.x, ball.position.y);
     return 0;
 }
