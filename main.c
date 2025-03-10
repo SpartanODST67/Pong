d34bc085c89548a46ca8dc40890b13d1da84a557
Hans Landgrebe <hlxbl20@gmail.com>
2025-03-10 13:32:28 -0500

    Ball now takes it dimensions in account

--- main.c
+++ main.c
@@ -50,21 +50,21 @@ Vector2 moveBall(Block* ball, Block leftPaddle, Block rightPaddle, Vector2 gameB
     ball->position.x += ball->velocity.x;
     ball->position.y += ball->velocity.y;
 
-    if(ball->position.x <= 0) {
-        ball->position.x = 0;
+    if(ball->position.x - (ball->dimensions.x / 2) <= 0) { //If ball collides with the left wall.
+        ball->position.x = ball->dimensions.x / 2;
         ball->velocity.x = -1 * ball->velocity.x;
     }
-    else if(ball->position.x >= gameBorder.x) {
-        ball->position.x = gameBorder.x;
+    else if(ball->position.x + (ball->dimensions.x / 2) >= gameBorder.x) { //If ball collides with the right wall.
+        ball->position.x = gameBorder.x - (ball->dimensions.x / 2);
         ball->velocity.x = -1 * ball->velocity.x;
     }
 
-    if(ball->position.y <= 0) {
-        ball->position.y = 0;
+    if(ball->position.y - (ball->dimensions.y / 2) <= 0) { //If ball collides with the bottom wall.
+        ball->position.y = ball->dimensions.y / 2;
         ball->velocity.y = -1 * ball->velocity.y;
     }
-    else if(ball->position.y >= gameBorder.y) {
-        ball->position.y = gameBorder.y;
+    else if(ball->position.y + (ball->dimensions.y / 2) >= gameBorder.y) { // if ball collides with the top wall.
+        ball->position.y = gameBorder.y - (ball->dimensions.y / 2);
         ball->velocity.y = -1 * ball->velocity.y;
     }
 
