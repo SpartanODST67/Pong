9b121c4473f5d93dd403513fd0b22b95bfc4a0fa
Hans Landgrebe <hlxbl20@gmail.com>
2025-03-10 13:26:23 -0500

    Ball now bounces on the game borders

--- main.c
+++ main.c
@@ -8,7 +8,10 @@ struct Vector2 {
 struct Block {
     Vector2 position;
     Vector2 dimensions;
+    Vector2 velocity;
 } typedef Block;
+
+Vector2 moveBall(Block*, Block, Block, Vector2);
 
 int main(int argc, char* argv[]) {
 
@@ -21,6 +24,8 @@ int main(int argc, char* argv[]) {
     ball.dimensions.y = 10;
     ball.position.x = (gameBorder.x / 2);
     ball.position.y = (gameBorder.y / 2);
+    ball.velocity.x = 5;
+    ball.velocity.y = 5;
 
     Block leftPaddle;
     leftPaddle.dimensions.x = 20;
@@ -34,6 +39,34 @@ int main(int argc, char* argv[]) {
     rightPaddle.position.x = gameBorder.x - 40;
     rightPaddle.position.y = gameBorder.y / 2;
 
-    printf("Game dimensions: %fx%f, Ball at: (%f, %f)", gameBorder.x, gameBorder.y, ball.position.x, ball.position.y);
+    for(int i = 0; i < 100; i++) {
+        ball.position = moveBall(&ball, leftPaddle, rightPaddle, gameBorder);
+        printf("Game dimensions: %fx%f, Ball at: (%f, %f)\n", gameBorder.x, gameBorder.y, ball.position.x, ball.position.y);
+    }
     return 0;
+}
+
+Vector2 moveBall(Block* ball, Block leftPaddle, Block rightPaddle, Vector2 gameBorder) {
+    ball->position.x += ball->velocity.x;
+    ball->position.y += ball->velocity.y;
+
+    if(ball->position.x <= 0) {
+        ball->position.x = 0;
+        ball->velocity.x = -1 * ball->velocity.x;
+    }
+    else if(ball->position.x >= gameBorder.x) {
+        ball->position.x = gameBorder.x;
+        ball->velocity.x = -1 * ball->velocity.x;
+    }
+
+    if(ball->position.y <= 0) {
+        ball->position.y = 0;
+        ball->velocity.y = -1 * ball->velocity.y;
+    }
+    else if(ball->position.y >= gameBorder.y) {
+        ball->position.y = gameBorder.y;
+        ball->velocity.y = -1 * ball->velocity.y;
+    }
+
+    return ball->position;
 }
