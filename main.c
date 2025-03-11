39d2cdc3940b226a910b2a78b000fd86768fea83
Hans Landgrebe <hlxbl20@gmail.com>
2025-03-11 14:31:43 -0500

    Learned more about WindProc messages

--- main.c
+++ main.c
@@ -126,26 +126,31 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 //  CALLBACK: calling convention. 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
     switch(msg) {
-        case WM_CLOSE:
+        case WM_CLOSE: // When user clicks the X or CTRL + F4.
+            //Default wind proc behaviour already does this.
             DestroyWindow(hwnd);
             break;
-        case WM_DESTROY:
+        case WM_DESTROY: // When window is no longer visible but is about to be destroyed (before any child windows are destroyed).
             PostQuitMessage(0); //Used to break out of message loop.
             break;
-        case WM_PAINT:
+        case WM_PAINT: //When a portion of the window needs to be repainted.
         {
             PAINTSTRUCT ps;
-            HDC screen = BeginPaint(hwnd, &ps);
+            //Fills ps with info on repaint request.
+            HDC screen = BeginPaint(hwnd, &ps); 
 
             //All painting here.
             drawScreen(screen, gameBorder, ball, leftPaddle, rightPaddle);
 
+            //Clears update region.
             EndPaint(hwnd, &ps);
         }
         break;
+        
         default:
         return DefWindowProc(hwnd, msg, wParam, lParam); // Performs default action to message.
     }
+
     return 0;
 }
 
