8f9db61fdda635c0a3f3e139c52aa16ec95df213
Hans Landgrebe <hlxbl20@gmail.com>
2025-03-14 13:15:11 -0500

    Thread now use gamestate alloc

--- main.c
+++ main.c
@@ -13,16 +13,12 @@ struct GameStateInfo {
 } typedef GameStateInfo;
 
 DWORD WINAPI gameLoop(LPVOID);
-void drawScreen(HDC, Block, Block, Block);
+void drawScreen(HDC, GameStateInfo);
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 GameStateInfo* GetAppState(HWND);
-void handleInput(char, Block*, Block*);
+void handleInput(char, Block*, Block*, Vector2);
 
 const char windowClassName[] = "PongWindowClass";
-Vector2 gameBorder;
-Block ball;
-Block leftPaddle;
-Block rightPaddle;
 HWND hwnd;
 
 /// @brief Entry point of Windows Program.
@@ -35,20 +31,13 @@ HWND hwnd;
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
     HANDLE heap = GetProcessHeap();
 
-    GameStateInfo* gameState = HeapAlloc(heap, 0, sizeof(GameStateInfo)); //Allocate Memory
+    GameStateInfo* gameState = HeapAlloc(heap, HEAP_ZERO_MEMORY, sizeof(GameStateInfo)); //Allocate Memory
     gameState->gameBorder.x = 960;
     gameState->gameBorder.y = 520;
 
     gameState->ball = createBall(gameState->gameBorder.x / 2, gameState->gameBorder.y / 2, 0);
     gameState->leftPaddle = createPaddle(gameState->gameBorder, 40, -1);
     gameState->rightPaddle = createPaddle(gameState->gameBorder, 40, 1);
-
-    gameBorder.x = 960;
-    gameBorder.y = 520;
-
-    ball = createBall(gameBorder.x / 2, gameBorder.y / 2, 0);
-    leftPaddle = createPaddle(gameBorder, 40, -1);
-    rightPaddle = createPaddle(gameBorder, 40, 1);
 
     WNDCLASSEX wc;
     MSG msg;
@@ -84,8 +73,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
         //Size and position
         CW_USEDEFAULT,     
         CW_USEDEFAULT,     
-        gameBorder.x + 20, // Width
-        gameBorder.y + 45, // Height
+        gameState->gameBorder.x + 20, // Width
+        gameState->gameBorder.y + 45, // Height
         
         NULL,       // Parent Window (Null for top level windows)
         NULL,       // Menu
@@ -161,7 +150,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
             PostQuitMessage(0); //Used to break out of message loop.
             break;
         case WM_CHAR:
-            handleInput((char) wParam, &(gameState->leftPaddle), &(gameState->rightPaddle));
+            handleInput((char) wParam, &(gameState->leftPaddle), &(gameState->rightPaddle), gameState->gameBorder);
             break;
         case WM_PAINT: //When a portion of the window needs to be repainted.
         {
@@ -170,7 +159,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
             HDC screen = BeginPaint(hwnd, &ps); 
 
             //All painting here.
-            drawScreen(screen, gameState->ball, gameState->leftPaddle, gameState->rightPaddle);
+            drawScreen(screen, *gameState);
 
             //Clears update region.
             EndPaint(hwnd, &ps);
@@ -194,25 +183,24 @@ inline GameStateInfo* GetAppState(HWND hwnd) {
 /// @param lpParam Thread data that is passed during thread creation.
 /// @return Completion status of the thread.
 DWORD WINAPI gameLoop(LPVOID lpParam) {
-    int player1Score = 0;
-    int player2Score = 0;
+    GameStateInfo* gameState = (GameStateInfo*)lpParam;
     int scored = 0;
     
-    while(player1Score < 10 && player2Score < 10) {
-        ball.position = moveBall(&ball, leftPaddle, rightPaddle, gameBorder);
+    while(gameState->player1Score < 10 && gameState->player2Score < 10) {
+        gameState->ball.position = moveBall(&(gameState->ball), gameState->leftPaddle, gameState->rightPaddle, gameState->gameBorder);
     
-        if(ball.position.x - (ball.dimensions.x / 2) <= 0) {
-            player2Score++;
+        if(gameState->ball.position.x - (gameState->ball.dimensions.x / 2) <= 0) {
+            gameState->player2Score++;
             scored = 2;
         }
-        else if(ball.position.x + (ball.dimensions.x / 2) >= gameBorder.x) {
-            player1Score++;
+        else if(gameState->ball.position.x + (gameState->ball.dimensions.x / 2) >= gameState->gameBorder.x) {
+            gameState->player1Score++;
             scored = 1;
         }
 
         if(scored != 0) {
-            if(scored == 1) ball = createBall(gameBorder.x / 2, gameBorder.y / 2, -1);
-            else ball = createBall(gameBorder.x / 2, gameBorder.y / 2, 1);
+            if(scored == 1) gameState->ball = createBall(gameState->gameBorder.x / 2, gameState->gameBorder.y / 2, -1);
+            else gameState->ball = createBall(gameState->gameBorder.x / 2, gameState->gameBorder.y / 2, 1);
             scored = 0;
         }
         
@@ -222,7 +210,7 @@ DWORD WINAPI gameLoop(LPVOID lpParam) {
     }
 
     char* victoryText;
-    if(player1Score > player2Score) victoryText = "Player 1 Wins!";
+    if(gameState->player1Score > gameState->player2Score) victoryText = "Player 1 Wins!";
     else victoryText = "Player 2 Wins!";
 
     while(MessageBox(hwnd, victoryText, "Game Over!", MB_ICONEXCLAMATION | MB_OK) != IDOK);
@@ -231,7 +219,7 @@ DWORD WINAPI gameLoop(LPVOID lpParam) {
     return 0;
 }
 
-void handleInput(char pushedKey, Block* leftPaddle, Block* rightPaddle) {
+void handleInput(char pushedKey, Block* leftPaddle, Block* rightPaddle, Vector2 gameBorder) {
     if(pushedKey == PLAYER_ONE_UP || pushedKey == PLAYER_ONE_DOWN) leftPaddle->position = movePaddle(pushedKey, *leftPaddle, gameBorder);
     else if(pushedKey == PLAYER_TWO_UP || pushedKey == PLAYER_TWO_DOWN) rightPaddle->position = movePaddle(pushedKey, *rightPaddle, gameBorder);
 }
@@ -241,19 +229,23 @@ void handleInput(char pushedKey, Block* leftPaddle, Block* rightPaddle) {
 /// @param ball Struct that stores the ball's information.
 /// @param leftPaddle Struct that stores the left paddle's information.
 /// @param rightPaddle Struct that stores the right paddle's information.
-void drawScreen(HDC screen, Block ball, Block leftPaddle, Block rightPaddle) {
+void drawScreen(HDC screen, GameStateInfo gameState) {
     HBRUSH brush = CreateSolidBrush(RGB(255, 255, 255));
     SelectObject(screen, brush);
 
     //Draw Ball
-    Rectangle(screen, ball.position.x - (ball.dimensions.x / 2), (ball.position.y + ball.dimensions.y / 2),
-                ball.position.x + (ball.dimensions.x / 2), ball.position.y - (ball.dimensions.y / 2));
+    Rectangle(screen, gameState.ball.position.x - (gameState.ball.dimensions.x / 2), (gameState.ball.position.y + gameState.ball.dimensions.y / 2),
+                gameState.ball.position.x + (gameState.ball.dimensions.x / 2), gameState.ball.position.y - (gameState.ball.dimensions.y / 2));
 
     //Draw Left Paddle
-    Rectangle(screen, leftPaddle.position.x - (leftPaddle.dimensions.x / 2), (leftPaddle.position.y + leftPaddle.dimensions.y / 2),
-                leftPaddle.position.x + (leftPaddle.dimensions.x / 2), leftPaddle.position.y - (leftPaddle.dimensions.y / 2));
+    Rectangle(screen, gameState.leftPaddle.position.x - (gameState.leftPaddle.dimensions.x / 2), 
+                gameState.leftPaddle.position.y + (gameState.leftPaddle.dimensions.y / 2),
+                gameState.leftPaddle.position.x + (gameState.leftPaddle.dimensions.x / 2), 
+                gameState.leftPaddle.position.y - (gameState.leftPaddle.dimensions.y / 2));
 
     //Draw Right Paddle
-    Rectangle(screen, rightPaddle.position.x - (rightPaddle.dimensions.x / 2), (rightPaddle.position.y + rightPaddle.dimensions.y / 2),
-        rightPaddle.position.x + (rightPaddle.dimensions.x / 2), rightPaddle.position.y - (rightPaddle.dimensions.y / 2));
+    Rectangle(screen, gameState.rightPaddle.position.x - (gameState.rightPaddle.dimensions.x / 2), 
+                gameState.rightPaddle.position.y + (gameState.rightPaddle.dimensions.y / 2),
+                gameState.rightPaddle.position.x + (gameState.rightPaddle.dimensions.x / 2), 
+                gameState.rightPaddle.position.y - (gameState.rightPaddle.dimensions.y / 2));
 }
